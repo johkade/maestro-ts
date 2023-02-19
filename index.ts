5ad6466f5808f7ac03e79ea974ac89b18844f4c9
johkade <johkade.dev@gmail.com>
2023-02-19 12:36:50 +0100

    chore: test

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log("hello world what's up?")
+import * as fs from "fs"
+
+console.log("Scanning for *.maestro.ts files")
+
+const files = {
+  flows: [] as string[],
+  other: [] as string[],
+}
+
+fs.readdirSync(".").forEach((file) => {
+  if (file.includes(".maestro.")) return files.flows.push(file)
+
+  return files.other.push(file)
+})
+
+console.log(`Found flows: ${files.flows.length}`)
+console.log(`Found others: ${files.other.length}`)
