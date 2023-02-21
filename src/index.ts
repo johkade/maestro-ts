41101e1801b1d3e89cb3ad6cc227b1f514c0554b
johkade <johkade.dev@gmail.com>
2023-02-21 23:27:18 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ const main = () => {
         "  console.log('did not find extension')\n" +
         "}\n" +
         '\nconst fs = require("fs")\n' +
-        'const { MaestroTranslators } = require("../build/src/commands")\n' +
+        'const { MaestroTranslators } = require("../src/commands")\n' +
         "const M = {...MaestroTranslators, ...MExt}\n" +
         'let out = ""\n'
     )
